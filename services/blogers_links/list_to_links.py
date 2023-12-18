eb9fc9b43247ac96009cc47e5e51ad0b9f950a46
ilia_volodin <vollodin61@gmail.com>
2023-12-18 16:31:25 +0300

    коммиитт

--- services/blogers_links/list_to_links.py
+++ services/blogers_links/list_to_links.py
@@ -1,4 +1,8 @@
 import re
 
-with open("list.txt", "r", encoding="utf-8") as lst, open("link.txt", "a+", encoding="utf-8") as lnk:
+FROM = '/home/i/MyPros/F/All_utils/All_utils_project/services/blogers_links/FROM.txt'
+TO = '/home/i/MyPros/F/All_utils/All_utils_project/services/blogers_links/TO.txt'
+
+with (open(FROM, "r", encoding="utf-8") as lst,
+	  open(TO, "w+", encoding="utf-8") as lnk):
 	[lnk.write(re.sub(r"\d+. ", '', f"{i[:-25]}\n")) for i in set(lst.readlines()) if i.strip()]
